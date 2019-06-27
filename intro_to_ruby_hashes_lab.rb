6a06d7db7214c190bc85aa7500fb5799f600d185
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 14:59:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,10 +22,10 @@ def monopoly_with_second_tier
   :railroads=>
     {
     :pieces=>4,
-    :rent_in_dollars=>
+    :names=>
       {
       },
-    :names=>
+    :rent_in_dollars=>
       {
       }
     }
